d4d487bb19ff39d7a5d29fb481078bb1f3a2312b
hideaki murotani <munroyello@gmail.com>
2020-08-14 17:28:15 +0900

    💩アスペクト比調整

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,92 @@ export default class App extends Vue {
 
   public async download() {
     console.log("ダウンロード開始");
-    const htmlImageElements = this.getHTMLImageElements();
-    const srces = htmlImageElements.map((el) => el.src);
-    // window.open(srces[0]);
-    const img = await fetch(srces[0]);
-    const blob = await img.blob();
-    var reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = function () {
-      var base64data = reader.result;
-      console.log(base64data);
-      const doc = new jsPDF();
-      doc.setFontSize(40);
-      const title = "download test";
-      console.log(title);
-      doc.text(title, 15, 15);
-      doc.addImage(base64data as string, "jpg", 30, 30, 100, 100);
-      doc.save("pixiv.pdf");
-    };
+    const srces = this.getHTMLImageElements();
+
+    console.log(srces[0]);
+
+    let pdfname = Date.now() + ".pdf";
+    const doc = new jsPDF("p", "mm", "a4");
+    doc.setFontSize(40);
+    let x = 0;
+    let y = 0;
+    const title = "title";
+
+    let url = srces[0].src.split("p0");
+    console.log("url:" + url);
+
+    console.log("pages:" + srces.length);
+    for (let i = 0, len = srces.length; 1; i++) {
+      console.log("now:" + i);
+      let url_p = url[0] + "p" + String(i) + url[1];
+      const img = await fetch(url_p);
+      //window.open(url_p)
+      const blob = await img.blob();
+      console.log("page:" + i + ", blob size:" + blob.size);
+      if (blob.size == 0) {
+        console.log("!!!!!!!!!!!!");
+        break;
+      }
+
+      var reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = function () {
+        var base64data = reader.result;
+        const img = new Image();
+        img.onload = function () {
+          console.log(img.width, img.height);
+          const asp = img.height / img.width;
+          const pageWidth = doc.internal.pageSize.getWidth();
+          const pageHeight = doc.internal.pageSize.getHeight();
+          // ページの高さをオーバーしてしまう場合
+          if (pageWidth * asp > pageHeight) {
+            doc.addImage(
+              base64data as string,
+              "jpg",
+              x,
+              y,
+              pageHeight / asp,
+              pageHeight
+            );
+          } else {
+            doc.addImage(
+              base64data as string,
+              "jpg",
+              x,
+              y,
+              pageWidth,
+              pageWidth * asp
+            );
+          }
+          doc.addPage();
+        };
+        img.src = base64data as string;
+      };
+    }
+    doc.save(pdfname);
   }
+
+  // public async download() {
+  //   console.log("ダウンロード開始");
+  //   const htmlImageElements = this.getHTMLImageElements();
+  //   const srces = htmlImageElements.map((el) => el.src);
+  //   // window.open(srces[0]);
+  //   const img = await fetch(srces[0]);
+  //   const blob = await img.blob();
+  //   var reader = new FileReader();
+  //   reader.readAsDataURL(blob);
+  //   reader.onloadend = function () {
+  //     var base64data = reader.result;
+  //     console.log(base64data);
+  //     const doc = new jsPDF();
+  //     doc.setFontSize(40);
+  //     const title = "download test";
+  //     console.log(title);
+  //     doc.text(title, 15, 15);
+  //     doc.addImage(base64data as string, "jpg", 30, 30, 100, 100);
+  //     doc.save("pixiv.pdf");
+  //   };
+  // }
 
   // すべて見るをクリック
   public clickAllView() {
--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,4 @@ window.onload = async () => {
       render: (h) => h(App),
     }).$mount('#app');
   }
-
-  if (/i.pximg.net/.test(location.href)) {
-    console.log("画像のBASE64化を開始します");
-    const img = document.querySelector("img");
-    console.log(img);
-    const base64 = getBase64Image(img!);
-    console.log(base64)
-  }
-
 };
-
-function getBase64Image(img: HTMLImageElement) {
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext("2d");
-  ctx?.drawImage(img, 0, 0);
-  var dataURL = canvas.toDataURL("image/png");
-  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
